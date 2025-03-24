c3441a74134d36e64e07fb4608f66af8aa61b507
bspat <154008056+bspat@users.noreply.github.com>
2025-03-24 22:27:16 +0100

    Naprawiono klase Wieza

--- Shogi/src/bierki/PromowalnaBierka.java
+++ Shogi/src/bierki/PromowalnaBierka.java
@@ -1,31 +1,27 @@
 package bierki;
 
-public abstract class PromowalnaBierka extends Bierka{
+public abstract class PromowalnaBierka extends Bierka {
     boolean promowana = false;
 
     //przy promocji zmienia status pionka
-    public void promocja()
-    {
+    public void promocja() {
         promowana = true;
     }
 
     protected abstract boolean czyLegalnyPrzedPromocja(int x, int y);
 
     //pomocnicza funkcja dla klas, ktore zmieniaja ruch po promocji - domyslnie zaimplementowan ruch jak dla zlotego generala
-    protected boolean czyLegalnyPoPromocji(int x, int y)
-    {
+    protected boolean czyLegalnyPoPromocji(int x, int y) {
         boolean legalny = false;
-        int[][] mozliwe = {{-1, 1},   {0,  1},  {1, 1},
-                           {-1, 0} ,/*{0,  0},*/{1, 0},
-                         /*{-1, -1},*/{0, -1},/*{1, -1}*/};
+        int[][] mozliwe = {{-1, 1}, {0, 1}, {1, 1},
+                {-1, 0},/*{0,  0},*/{1, 0},
+                /*{-1, -1},*/{0, -1},/*{1, -1}*/};
         int k = (kolor == Kolor.BIALY) ? 1 : -1;
-        if(promowana)
-        {
-            for (int[] ints : mozliwe) {
-                if (((wiersz + ints[0]) == x) && ((kolumna + ints[1]) == y)) {
-                    legalny = true;
-                    break;
-                }
+
+        for (int[] ints : mozliwe) {
+            if (((wiersz + ints[0]) == x) && ((kolumna + ints[1]) == y)) {
+                legalny = true;
+                break;
             }
         }
         return legalny;
@@ -33,16 +29,14 @@ public abstract class PromowalnaBierka extends Bierka{
 
     @Override
     public boolean czyLegalny(int x, int y) {
-        if(promowana)
-        {
-            if(czyLegalnyPoPromocji(x, y)) return true;
+        if (promowana) {
+            if (czyLegalnyPoPromocji(x, y)) return true;
         }
         return czyLegalnyPrzedPromocja(x, y);
     }
 
     @Override
-    public void zbity()
-    {
+    public void zbity() {
         promowana = false;
         super.zbity();
     }
--- Shogi/src/bierki/Wieza.java
+++ Shogi/src/bierki/Wieza.java
@@ -1,6 +1,6 @@
 package bierki;
 
-public class Wieza extends PromowalnaBierka{
+public class Wieza extends PromowalnaBierka {
 
 
     protected Wieza(Kolor kolor, Stan stan, int wiersz, int kolumna) {
@@ -10,12 +10,17 @@ public class Wieza extends PromowalnaBierka{
 
     @Override
     protected boolean czyLegalnyPrzedPromocja(int x, int y) {
-        return false;
+        return (x == this.kolumna) || (y == this.wiersz);
     }
 
     @Override
-    protected boolean czyLegalnyPoPromocji(int x, int y)
-    {
+    protected boolean czyLegalnyPoPromocji(int x, int y) {
+        int[][] mozliwe = {{-1, 1},   /*{0,  1},*/  {1, 1},
+                /* {-1, 0} ,{0,  0}, {1, 0},*/
+                {-1, -1}, /*{0, -1}, */{1, -1}};
+        for (int[] ints : mozliwe) {
+            if (((wiersz + ints[0]) == x) && ((kolumna + ints[1]) == y)) return true;
+        }
         return false;
     }
 }
