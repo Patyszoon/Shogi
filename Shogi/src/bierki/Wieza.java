a6772f70e3389c8b0e8000057cfb1517aefc5409
bspat <154008056+bspat@users.noreply.github.com>
2025-03-24 11:18:58 +0100

    Dodano klase Wieza

--- Shogi/src/bierki/Wieza.java
+++ Shogi/src/bierki/Wieza.java
@@ -7,11 +7,23 @@ public class Wieza extends PromowalnaBierka{
         super(kolor, stan, wiersz, kolumna);
     }
 
+
     @Override
-    public boolean czyLegalny(int x, int y) {
-        if((x == this.kolumna)||(y == this.wiersz))
+    protected boolean czyLegalnyPoPromocji(int x, int y)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean czyLegalny(int x, int y)
+    {
+        if(promowana)
         {
-            return true;
+            if(czyLegalnyPoPromocji(x, y)) return true;
+        }
+        if ((x == this.kolumna) || (y == this.wiersz))
+        {
+                return true;
         }
         return false;
     }
